0facc2330ab7a54a95ce201abea9530683175502
Nosib Biswas <88088042+NOSIBBiswas22@users.noreply.github.com>
2025-11-05 21:11:28 +0600

    deal with some critical cases

--- class_problems/secondMax.c
+++ class_problems/secondMax.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main() {
-    int n, max=0, second_max=0;
+    int n, max, second_max;
     scanf("%d", &n);
     
     int nums[n];
@@ -9,16 +9,17 @@ int main() {
         scanf("%d", &nums[i]);
     }
     max=nums[0];
+    second_max=nums[0];
     
     for(int i=1; i<n; i++){
         if(max<nums[i]){
             second_max=max;
             max=nums[i];
-        }else if(second_max<nums[i]){
+        }else if(second_max<nums[i] && max > nums[i]){
             second_max=nums[i];
         }
     }
-    printf("%d %d", second_max, max);
+    printf("%d\n", second_max);
     
     return 0;
 }
